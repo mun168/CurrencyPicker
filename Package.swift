a0efe08364d4d72ca03f480728d0f86d5ed40ca8
mun168 <67210857+mun168@users.noreply.github.com>
2024-06-25 12:05:30 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,11 +11,15 @@ let package = Package(
             name: "CurrencyPicker",
             targets: ["CurrencyPicker"]),
     ],
+    dependencies: [
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CurrencyPicker"),
+            dependencies: [],
+            resources: [.copy("Resources/currencies.json")]
         .testTarget(
             name: "CurrencyPickerTests",
             dependencies: ["CurrencyPicker"]),
